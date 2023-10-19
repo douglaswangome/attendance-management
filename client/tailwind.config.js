b724b00d54dcd0d91aa1cc1534f27fd90f875cbf
Douglas Wangome <ishdagnesh@gmail.com>
2023-10-19 21:24:36 +0300

    Project Initialization

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        // Primary
+        primary: "#407BFF",
+        // Light
+        // Dark
+      },
+    },
+  },
+  plugins: [],
+};
